b5e171c18dae88d6954c5ff5badf607dba2b5de7
Nguyen Cong Trang <105571594+congtrang45@users.noreply.github.com>
2024-02-19 13:53:21 +0700

    Update main_raspberry.c

--- main_raspberry.c
+++ main_raspberry.c
@@ -1,12 +1,12 @@
 #include <string.h>
 #include <errno.h>
-#include <wiringSerial.h>
 #include <wiringPi.h>
+#include <wiringSerial.h>
 #include <wiringPiI2C.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>   
-#include <math.h>    //Compile : gcc -o a a.c -l wiringPi -lm
+#include <math.h>   
 
 #define Sample_rate 25
 #define Config      26
@@ -17,38 +17,32 @@
 #define Acc_X       59
 #define Acc_Y       61
 #define Acc_Z       63
+#define Gyro_X		67
+#define Gyro_Y		69
+#define Gyro_Z		71
+#define center_bt   12
 
-#define BT_LG           25      // landing gear
-#define BT_FU           20      // flap up
-#define BT_FD           16      // flap down
-#define BT_STOP         26      // tam dung
-#define BT_UG           19      // up gas
-#define BT_DG           13      // down gas
-#define BT_EXIT         5       // thoat game
-#define BT_AUTO         6       // tu dong lai
-#define BT_R            8      // quay phai
-#define BT_L            21       // quay trai
-
-unsigned int a,b,c,d,e,f,g,td,i,k = 0;
-int mpu;
+int mpu, center;
 float data;
-
+float deltaT = 0.1; // Update time
+volatile float Gx, Gy, prePitch, preRoll, pitch, roll;
 void Init_6050(void){
         // register 25->28,56,107
-        // sample_rate 500hz
-        wiringPiI2CWriteReg8(mpu,Sample_rate,15);
-        //ko sd nguồn xung ngoài, tắt DLFP
-        wiringPiI2CWriteReg8(mpu,Config,0);
+        // Sample Rate: 500hz
+        wiringPiI2CWriteReg8(mpu, Sample_rate, 15);
+        //Not use the external Frame Synchronization, turn of DLPF
+        wiringPiI2CWriteReg8(mpu, Config, 0);
         //gyro FS: +-500 o/s
-        wiringPiI2CWriteReg8(mpu,Gyro_config,0x08);
-        //acc FS: +- 8g
-        wiringPiI2CWriteReg8(mpu,Acc_config,0x10);
-        // mở interrupt của data ready
-        wiringPiI2CWriteReg8(mpu,Interrupt,1);
-        // chọn nguồn xung Gyro X
-        wiringPiI2CWriteReg8(mpu,PWR_managment,0x01);
+        wiringPiI2CWriteReg8(mpu, Gyro_config, 0x08);
+        //accel FS: +- 8g
+        wiringPiI2CWriteReg8(mpu, Acc_config, 0x10);
+        //Open interrupt of Data ready
+        wiringPiI2CWriteReg8(mpu, Interrupt, 1);
+        //Select Clock Source Gyro X
+        wiringPiI2CWriteReg8(mpu, PWR_managment, 0x01);
 }
 
+// Read sensor Function with int16_t data type
 int16_t read_sensor(unsigned char sensor)
 {
     int16_t high,low,data;
@@ -57,118 +51,86 @@ int16_t read_sensor(unsigned char sensor)
     data = (high<<8) | low;
     return data;
 }
-
-void read_button(){
-        // nut nhan landing gear
-        if(!digitalRead(BT_LG)){
-                a = 1;              
-        }
-        else{   a = 0;}
-        // nut nhan flap up
-        if(!digitalRead(BT_FU)){
-                b = 1;              
-        }
-        else{   b = 0;}
-        // nut nhan flap down
-        if(!digitalRead(BT_FD)){
-                c = 1;              
-        }
-        else{   c = 0;}
-        // nut nhan stop
-        if(!digitalRead(BT_STOP)){
-                d = 1;              
-        }
-        else{   d = 0;}
-        // nut nhan up gas
-        if(!digitalRead(BT_UG)){
-                e = 1;              
-        }
-        else{   e = 0;}
-        // nut nhan down gas
-        if(!digitalRead(BT_DG)){
-                f = 1;              
-        }
-        else{   f = 0;}
-        // nut nhan exit 
-        if(!digitalRead(BT_EXIT)){
-                g = 1;              
-        }
-        else{   g = 0;}
-        // nut nhan che do lai auto 
-        if(!digitalRead(BT_AUTO)){
-                td = 1;              
-        }
-        else{   td = 0;}
-        // quay phai
-        if(!digitalRead(BT_R)){
-                i = 1;              
-        }
-        else{   i = 0;}
-        // quay trai
-        if(!digitalRead(BT_L)){
-                k = 1;              
-        }
-        else{   k = 0;}
-
-        delay(1);
+//Update Gyroscope value Function
+float UpdateGyro(unsigned char G)
+{
+	delay(deltaT*1000);
+	switch (G)
+	{
+		case 'X':
+			Gx = (float)read_sensor(Gyro_X)/65.5;
+			break;  
+		default:
+			Gy = (float)read_sensor(Gyro_Y)/65.5;
+			break;
+	}
+}
+//*****************************************************************//
+/*Pitch = Integral from 0 to t of Gyroscope Y
+Because when the sensor moves, the value of Gyroscope Y
+is a discrete value, so to calculate the fraction, calculate
+the area under the graph by summing the areas of the trapezoid.*/
+//****************************************************************//
+// Function to calculate new pitch angle based on previous pitch and gyro value
+float CalculatePitch(float prePitch, float preGyro, float curGyro, float deltaT)
+{
+	return prePitch + 0.5*(preGyro + curGyro)*deltaT;
+}
+// Function to calculate new roll angle based on previous roll and gyro value
+float CalculateRoll(float preRoll, float preGyro, float curGyro, float deltaT)
+{
+	return preRoll + 0.5*(preGyro + curGyro)*deltaT;
 }
 
-int main(void)
+int main()
 {
-    mpu = wiringPiI2CSetup(0x68);
-    //setup gieo tiep i2c
+    //Setup GPIO Physical
     wiringPiSetupPhys();
-    wiringPiSetupGpio();
-    pinMode(BT_LG, INPUT);
-    pullUpDnControl (BT_LG, PUD_UP);
-    pinMode(BT_FU, INPUT);
-    pullUpDnControl (BT_FU, PUD_UP);
-    pinMode(BT_FD, INPUT);
-    pullUpDnControl (BT_FD, PUD_UP);
-    pinMode(BT_STOP, INPUT);
-    pullUpDnControl (BT_STOP, PUD_UP);
-    pinMode(BT_UG, INPUT);
-    pullUpDnControl (BT_UG, PUD_UP);
-    pinMode(BT_DG, INPUT);
-    pullUpDnControl (BT_DG, PUD_UP);
-    pinMode(BT_EXIT, INPUT);
-    pullUpDnControl (BT_EXIT, PUD_UP);
-    pinMode(BT_AUTO, INPUT);
-    pullUpDnControl (BT_AUTO, PUD_UP);
-    pinMode(BT_R, INPUT);
-    pullUpDnControl (BT_R, PUD_UP);
-    pinMode(BT_L, INPUT);
-    pullUpDnControl (BT_L, PUD_UP);
-
-    //thiet lap che do cho MPU
-    Init_6050();
-    
-    //đọc các giá trị đo
+	//Setup I2C Comunication
+    mpu = wiringPiI2CSetup(0x68);
+    //Set mode for MPU6050
+    Init_6050(); 
+    //Set mode for GPIO
+    pinMode(center_bt, INPUT);
+    //Read measured values
     while (1)
     {
+        //Get value of sensor when the sensor is stationary
+        // Accel value
         float Ax = (float)read_sensor(Acc_X)/4096.0;
         float Ay = (float)read_sensor(Acc_Y)/4096.0;
         float Az = (float)read_sensor(Acc_Z)/4096.0;
+		//Previous Pitch and Roll value
+        prePitch = atan2(-Ax,sqrt(pow(Ay,2)+pow(Az,2)))*180/M_PI;
+        preRoll = atan2(Ay,sqrt(pow(Ax,2)+pow(Az,2)))*180/M_PI;
+        //Get value of previous Gyroscope value
+        float preGx = (float)read_sensor(Gyro_X)/65.5;
+		float preGy = (float)read_sensor(Gyro_Y)/65.5;
+		// Calculate Pitch and Roll when the sensor is move
+		pitch = CalculatePitch(prePitch, preGx, UpdateGyro('Y'), deltaT);
+		roll = CalculateRoll(preRoll, preGy, UpdateGyro('X'), deltaT);
+        // Handle the center button
+        if (digitalRead(center_bt) == 1) {
+            center = 1;
+        }
+        else {
+            center = 0;
+        }
+        // Handle the UART
+        int fd;
+        if((fd = serialOpen ("/dev/ttyS0", 115200)) < 0 ){
+            fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno));
+            return 1;
+		}
+
         
-        float pitch = atan2(Ax,sqrt(pow(Ay,2)+pow(Az,2)))*180/M_PI;
-        float roll = atan2(Ay,sqrt(pow(Ax,2)+pow(Az,2)))*180/M_PI;
+        char cmd[200];
+		sprintf(cmd,"%.2f %.2f %d\n",pitch, roll, center);
+        printf("%s\n", cmd);
+        serialPrintf(fd, cmd);
 
-        read_button();
-
-	int fd;
-	if((fd = serialOpen ("/dev/ttyS0", 9600)) < 0 ){
-		fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno));
-	}
-
-    char cmd[200];
-    sprintf(cmd,"%d %d %d %d %d %d %d %.2f %.2f %d %d %d ",a,b,c,d,e,f,g,pitch,roll,td,i,k);
-    printf("%s\n", cmd);
-	serialPuts(fd, cmd);
-    delay(100);
-	serialFlush(fd);
-	fflush(stdout);
-	delay(100);
-	}
-    delay(1000);    
+        // Close serial port before exiting
+        serialClose(fd);
+	}   
     return 0;
 }
